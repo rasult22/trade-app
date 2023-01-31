1710cc03ec104fdab56b7d85cf5f2551786dfeb2
Itachi-MacOS <warrior.006@inbox.ru>
2023-02-01 04:05:30 +0600

    sd

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup lang="ts">
-import { useFirebase } from 'src/composables/useFirebase'
-useFirebase()
-</script>
--- src/pages/main-page/index-page.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="main-page full-width q-px-md">
-      main-page
-  </div>
-</template>
